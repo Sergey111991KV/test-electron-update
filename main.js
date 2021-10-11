2ceb048113696e5d5cb542b5b22ba061b914e690
Sergey111991KV <50921390+Sergey111991KV@users.noreply.github.com>
2021-10-11 21:48:37 +0300

    drag-and-drop
    drag-and-drop

---
+++ main.js
@@ -0,0 +1,48 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const { autoUpdater } = require('electron-updater');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+  }
+  })
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+  mainWindow.loadFile('index.html')
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
+
